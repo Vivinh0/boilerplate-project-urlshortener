93993813de5846fc979bf942b00f69163700a73c
Viviño <Vivinh0@users.noreply.github.com>
2021-01-11 19:49:51 +0100

    Add database setup and refactoring to test

--- tests/shorturl.test.js
+++ tests/shorturl.test.js
@@ -1,13 +1,11 @@
 "use strict";
 
+const mongoose = require("mongoose");
+const shorurlModel = require("../models/shorturlModel");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const expect = chai.expect;
 chai.use(chaiHttp);
-
-function objToString(object) {
-  return JSON.stringify(object, null, 2);
-}
 
 describe("Test URL Shortener Microservice", () => {
   let server;
@@ -17,6 +15,10 @@ describe("Test URL Shortener Microservice", () => {
     done();
   });
   afterEach((done) => server.close(() => done()));
+  after(async () => {
+    await shorurlModel.remove();
+    await mongoose.disconnect();
+  });
 
   describe("POST /api/shorturl/new", () => {
     it("should return a JSON response with keys original_url, with value 'https://freecodecamp.org', and short_url, with value 1: { original_url : 'https://freecodecamp.org', short_url : 1}", (done) => {
@@ -25,19 +27,17 @@ describe("Test URL Shortener Microservice", () => {
         .post("/api/shorturl/new")
         .send({ url_input: "https://www.freecodecamp.org/" })
         .end((err, res) => {
+          // console.log(res.body);
+          
           // Get results
-          const actualResult = res.body;
-          const expectedResult = {
-            original_url: "https://freecodecamp.org",
-            short_url: 1,
-          };
-
-          // Results to Strings
-          const actualResultToString = objToString(actualResult);
-          const expectedResultToString = objToString(expectedResult);
+          const actualOriginal_url = res.body.original_url;
+          const actualShort_url = Number.parseInt(res.body.short_url);
 
           // Test results
-          expect(actualResultToString).to.be.equal(expectedResultToString);
+          expect(actualOriginal_url).to.be.equal(
+            "https://www.freecodecamp.org/"
+          );
+          expect(actualShort_url).to.be.equal(1);
 
           done();
         });
